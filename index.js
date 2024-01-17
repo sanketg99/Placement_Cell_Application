bce94c73be3269289b50726d85078d89829ea99e
sanketg99 <sanketgorade99@gmail.com>
2024-01-17 10:55:29 +0530

    .

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import all required modules
 const express = require('express');
 const dotenv = require('dotenv');
 const db = require('./config/mongoose');
@@ -6,30 +7,30 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const port = process.env.PORT || 3200;
 
-
+// configuration of dotenv
 dotenv.config({ path: 'config/.env' });
 
 const app = express();
+
 // set ejs as view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(
-	session({
-		//change the secrate before deployment in production mode
-		secret: "secret", // SECRET is stored in the system veriable
-		//if the session data is alredy stored we dont need to rewrite it again and again so this is set to false
-		resave: false,
-		//when the user is not logged in or identity is not establish in that case we dont need to save extra data in
-		//session cookie so this is set to false
-		saveUninitialized: false,
-		cookie: { maxAge: 1000 * 60 * 100 },
-	})
+app.use(session({
+	secret: "secret", // SECRET is stored in the system veriable
+	//if the session data is alredy stored we dont need to rewrite it again and again so this is set to false
+	resave: false,
+	//when the user is not logged in or identity is not establish in that case we dont need to save extra data in
+	//session cookie so this is set to false
+	saveUninitialized: false,
+	cookie: { maxAge: 1000 * 60 * 100 },
+})
 );
 
 //extract styles and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 app.use(express.urlencoded({ extended: true }));
+
 //for static file use
 app.use(express.static('./assets'));
 
