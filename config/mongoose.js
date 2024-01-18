0299b9920760645ab380044e85e10d2f21f3134d
sanketg99 <sanketgorade99@gmail.com>
2024-01-18 11:40:12 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://sanketbg99:sanket123@cluster0.ewcrs64.mongodb.net/?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb://localhost:27017/Placement_Cell_App`);
 //mongodb://localhost:27017/Placement_Cell_App
+// mongodb+srv://sanketbg99:sanket123@cluster0.ewcrs64.mongodb.net/?retryWrites=true&w=majority/placementCell
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
